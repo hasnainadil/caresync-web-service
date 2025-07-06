10e23555ac80e312aab03db9b9f9a88286ff5dc4
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-06 21:58:33 +0600

    fix: api base urls

--- frontend/src/lib/api-urls.ts
+++ frontend/src/lib/api-urls.ts
@@ -7,9 +7,9 @@ import { Search } from "lucide-react";
 // };
 
 const API_BASE_URL = {
-  "location-service": "http://services.caresync.district12.xyz:8083",
-  "auth-service": "http://services.caresync.district12.xyz:8081",
-  "data-service": "http://services.caresync.district12.xyz:8082",
+  "location-service": "https://services.caresync.district12.xyz/location",
+  "auth-service": "https://services.caresync.district12.xyz/auth",
+  "data-service": "https://services.caresync.district12.xyz/data",
 };
 
 // const API_BASE_URL = {
